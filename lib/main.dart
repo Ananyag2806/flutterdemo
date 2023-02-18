e81fddc8651898b2a0e7b761ceeb51b03d35b759
Ananya Gupta <ananyag2806@gmail.com>
2023-02-19 01:06:54 +0530

    added cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:drag_and_drop_lists/drag_and_drop_lists.dart';
-import 'package:flutterdemo/navigation_drawer.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(ScrumBoard());
+void main() => runApp(const ScrumBoard());
 
 class ScrumBoard extends StatefulWidget {
   const ScrumBoard({Key? key}) : super(key: key);
@@ -37,19 +36,19 @@ class _ScrumBoard extends State<ScrumBoard> {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: const Text('Horizontal'),
+        title: const Text('Scrum4Everyday'),
       ),
       body: DragAndDropLists(
         children: List.generate(_lists.length, (index) => _buildList(index)),
         onItemReorder: _onItemReorder,
         onListReorder: _onListReorder,
         axis: Axis.horizontal,
-        listWidth: 150,
-        listDraggingWidth: 150,
-        listDecoration: BoxDecoration(
-          color: Colors.green[400],
-          borderRadius: const BorderRadius.all(Radius.circular(7.0)),
-          boxShadow: const <BoxShadow>[
+        listWidth: 330,
+        listDraggingWidth: 330,
+        listDecoration: const BoxDecoration(
+          color: Color(0xFF483838),
+          borderRadius: BorderRadius.all(Radius.circular(6.0)),
+          boxShadow: <BoxShadow>[
             BoxShadow(
               color: Colors.black45,
               spreadRadius: 3.0,
@@ -66,51 +65,6 @@ class _ScrumBoard extends State<ScrumBoard> {
   _buildList(int outerIndex) {
     var innerList = _lists[outerIndex];
     return DragAndDropList(
-      header: Row(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.vertical(top: Radius.circular(7.0)),
-                color: Colors.pink,
-              ),
-              padding: const EdgeInsets.all(10),
-              child: Text(
-                'Header ${innerList.name}',
-                style: Theme.of(context).primaryTextTheme.headline6,
-              ),
-            ),
-          ),
-        ],
-      ),
-      footer: Row(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              decoration: const BoxDecoration(
-                borderRadius:
-                    BorderRadius.vertical(bottom: Radius.circular(7.0)),
-                color: Colors.pink,
-              ),
-              padding: const EdgeInsets.all(10),
-              child: Text(
-                'Footer ${innerList.name}',
-                style: Theme.of(context).primaryTextTheme.headline6,
-              ),
-            ),
-          ),
-        ],
-      ),
-      leftSide: const VerticalDivider(
-        color: Colors.pink,
-        width: 1.5,
-        thickness: 1.5,
-      ),
-      rightSide: const VerticalDivider(
-        color: Colors.pink,
-        width: 1.5,
-        thickness: 1.5,
-      ),
       children: List.generate(innerList.children.length,
           (index) => _buildItem(innerList.children[index])),
     );
@@ -118,8 +72,30 @@ class _ScrumBoard extends State<ScrumBoard> {
 
   _buildItem(String item) {
     return DragAndDropItem(
-      child: ListTile(
-        title: Text(item),
+      child: Container(
+        height: 50,
+        margin: const EdgeInsets.all(8.0),
+        decoration: const BoxDecoration(
+          color: Color(0xFF483838),
+          borderRadius: BorderRadius.all(Radius.circular(6.0)),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: Colors.black45,
+              spreadRadius: 3.0,
+              blurRadius: 6.0,
+              offset: Offset(2, 3),
+            ),
+          ],
+        ),
+        child: Center(
+          child: Text(
+            item,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
+          ),
+        ),
       ),
     );
   }
@@ -139,3 +115,8 @@ class _ScrumBoard extends State<ScrumBoard> {
     });
   }
 }
+
+// black list color : 0xFF483838
+// text color : 
+// green background color:
+// green app bar color
